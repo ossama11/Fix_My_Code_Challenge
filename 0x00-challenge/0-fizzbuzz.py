ac22a3cf0d22eefced054d3cd99b8b1df235215e
ossama11 <jeuxossama67@gmail.com>
2024-02-24 12:31:53 +0100

    fix error

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -19,7 +19,8 @@ def fizzbuzz(n):
     tmp_result = []
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
+            # Change the logic here if desired
+            tmp_result.append("FizzBuzzNew")
         elif (i % 3) == 0:
             tmp_result.append("Fizz")
         elif (i % 5) == 0:
